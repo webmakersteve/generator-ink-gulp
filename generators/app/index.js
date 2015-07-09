d9bcb7eabf34ac78c7fa481fcb334fa4fc4bbbbc
Stephen Parente <stephen@ingenuitydesign.com>
2015-07-09 15:23:42 -0400

    Getting there...

--- generators/app/index.js
+++ generators/app/index.js
@@ -4,7 +4,7 @@ var chalk = require('chalk');
 var yosay = require('yosay');
 var request = require('request');
 
-const INK_VERSION = '1.0.5';
+var INK_VERSION = '1.0.5';
 
 module.exports = yeoman.generators.Base.extend({
   prompting: function () {
